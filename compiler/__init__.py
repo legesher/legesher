2dc7ad3ad31d4b3449c079dea994c2e62553070d
madipfaff <pfaffmadison@gmail.com>
2018-08-28 22:00:36 -0400

    add init

---
+++ compiler/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2018 With Madi Co. All Rights Reserved.
+#
+# Licensed under the MIT License;
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/MIT
+#
+# Happy Learning!!
